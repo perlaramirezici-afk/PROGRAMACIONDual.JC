b00db246c431cfde21d22a21458e50026d849997
Perla Ramirez <perlaramireziciQ@gmail.com>
2026-07-16 12:30:55 -0600

    realizo push

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btnSaludar").addEventListener("click", function (){ 
+    alert("¡Hola mundo!");
+   });
+
+document.getElementById("contacto").addEventListener("click", function () { 
+    console.log ("¡Hola Mundo!");
+    });
